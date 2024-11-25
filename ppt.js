33528c27879438a9f544322927f9ed9bae4f151e
HeinnerGN <heinnergil@gmail.com>
2024-11-24 19:21:27 -0500

    Update ppt.js

--- ppt.js
+++ ppt.js
@@ -1,31 +1,62 @@
 function registrarusuario(){
-    const nombre=document.getElementById("nombre"). value;
-    const contraseña=document.getElementById("password"). value;
+    const nombre=document.getElementById("nombre").value;
+    const contraseña=document.getElementById("password").value;
     if (!nombre||!contraseña){
         alert("Ingrese los requisitos por favor.");
         return;
     }
-    const usuario=JSON.parse(localStorage.getItem('usuarios'));
-    if('usuarios'[nombre]){
-        alert("Este usuario ya está registrado.");
+    const usuario=JSON.parse(localStorage.getItem('usuario')) || {};
+    if('usuario'[nombre]){
+        alert("Ingrese el nombre de usuario y la contraseña.");
     }
     else{
         usuario[nombre]=contraseña;
         localStorage.setItem('usuario', JSON.stringify(usuario));
         alert('El usuario ha sido registrado con éxito.');
-        document.getElementById("nombre").classList.add('hidden');
-        document.getElementById("contraseña").classList.remove('hidden');
+        document.getElementById("Registro").classList.add('hidden');
+        document.getElementById("Login").classList.remove('hidden');
     }
 }
 
 function iniciarsesion(){
-    const nombre=document.getElementById("nombre")
+    const nombre=document.getElementById('nombrelogin').value
+    const contraseña=document.getElementById('passwordlogin').value
+
+    if(!nombre||!contraseña){
+        alert("Ingrese los requisitos por favor.");
+        return;
+    }
+    const usuario=JSON.parse(localStorage.getItem('usuario')) ||{};
+
+    if(usuario[nombre] === contraseña){
+        alert("Bienvenido " + nombre + '!');
+        document.getElementById('Login').classList.add('hidden');
+        document.getElementById('Juego').classList.remove('hidden');
+    }
+    else{
+        alert("Usuario o contraseña incorrectos.")
+    }
 }
 
-function jugar(){
+function jugar(opcUsuario){
     const opciones=['Piedra', 'Papel', 'Tijeras'];
-    const opcionComputadora=opciones[Math.floor(Math.random() * opciones.length)];
+    const Computadora=opciones[Math.floor(Math.random() * opciones.length)];
 
     let resultado=' ';
     
+    if(opcUsuario === Computadora){
+        resultado="Empate" + "Ambos eligieron" `${opcUsuario}`;
+    }
+    else if(
+        (opcUsuario === 'Piedra' && Computadora === 'Tijeras') ||
+        (opcUsuario === 'Papel' && Computadora === 'Piedra') ||
+        (opcUsuario === 'Tijeras' && Computadora === 'Papel')
+    ){
+        resultado= `¡Has Ganado! ${opcUsuario} vence a ${Computadora}`;
+    }
+    else{
+        resultado= `Has perdido ${Computadora} vence a ${opcUsuario}`;
+    }
+
+    document.getElementById('resultado').innerText=`La computadora ha elegido ${Computadora}\n${resultado}`;
 }
